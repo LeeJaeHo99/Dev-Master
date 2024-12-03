5a33097af99340e9ff221f5b7abaacfe8ad3f5df
LeeJaeHo99 <apple@appleui-MacBookPro.local>
2024-12-04 01:25:03 +0900

    메인 섹션 레이아웃 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Header from '@/components/main/header/Header';
+import Header from "@/components/main/header/Header";
 
 export default function Home() {
     return (
-        <main className='w-full'>
-            <Header/>
+        <main className="w-full">
+            <Header />
+            <div className="mid-section flex items-center justify-between w-full pr-9">
+                <section className="calendar w-1/2">study</section>
+                <div className="mid-right--section flex items-center justify-between w-1/2">
+                    <div className="study border-2">study</div>
+                    <div className="test border-2">study</div>
+                </div>
+            </div>
+            <div className="bot-section flex items-center justify-between w-full pr-9">
+                <section className="calendar w-1/2">study</section>
+                <div className="bot-right--section flex items-center justify-between w-1/2">
+                    <div className="study border-2">study</div>
+                    <div className="test border-2">study</div>
+                </div>
+            </div>
         </main>
     );
 }
